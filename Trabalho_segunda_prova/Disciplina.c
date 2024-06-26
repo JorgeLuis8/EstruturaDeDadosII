38e61724c645a0cbccb461830dad3ba38a4ba806
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-06-26 14:02:14 -0300

    INSERCAO ESTA CORRETA NESSA DESGRACA

--- Trabalho_segunda_prova/Disciplina.c
+++ Trabalho_segunda_prova/Disciplina.c
@@ -11,6 +11,7 @@ arv_disciplina *cria_no()
     if (no != NULL)
     {
         no->dados = (dados_disciplina *)malloc(sizeof(dados_disciplina));
+        no->cor = RED;
         no->esq = NULL;
         no->dir = NULL;
     }
@@ -23,70 +24,49 @@ void ler_dados(arv_disciplina *no)
     printf("Informe o codigo da disciplina: ");
     scanf("%d", &no->dados->codigo);
 
-//     printf("Informe o nome da disciplina: ");
-//     scanf("%s", no->dados->nome);
+    // printf("Informe o nome da disciplina: ");
+    // scanf("%s", no->dados->nome);
 
-//     printf("Informe o bloco da disciplina: ");
-//     scanf("%d", &no->dados->bloco);
+    // printf("Informe o bloco da disciplina: ");
+    // scanf("%d", &no->dados->bloco);
 
-//     printf("Informe a carga-horaria da disciplina: ");
-//     scanf("%d", &no->dados->carga_horaria);
+    // printf("Informe a carga-horaria da disciplina: ");
+    // scanf("%d", &no->dados->carga_horaria);
 }
 
 // Função para trocar a cor dos nós
-void trocaCor(arv_disciplina *no)
+void trocaCor(arv_disciplina *H)
 {
-    if (no != NULL)
+    if (H != NULL)
     {
-        no->cor = (no->cor == RED) ? BLACK : RED;
+        H->cor = !H->cor;
+        if (H->esq != NULL)
+            H->esq->cor = !H->esq->cor;
+        if (H->dir != NULL)
+            H->dir->cor = !H->dir->cor;
     }
-}
-
-// Função para rotacionar um nó para a direita
-arv_disciplina *rotacionarDireita(arv_disciplina *no)
-{
-    arv_disciplina *aux = no->esq;
-    no->esq = aux->dir;
-    aux->dir = no;
-    return aux;
 }
 
 // Função para rotacionar um nó para a esquerda
-arv_disciplina *rotacionarEsquerda(arv_disciplina *no)
+arv_disciplina *rotacionarEsquerda(arv_disciplina *A)
 {
-    arv_disciplina *aux = no->dir;
-    no->dir = aux->esq;
-    aux->esq = no;
-    return aux;
+    arv_disciplina *B = A->dir;
+    A->dir = B->esq;
+    B->esq = A;
+    B->cor = A->cor;
+    A->cor = RED;
+    return B;
 }
 
-arv_disciplina *balancear(arv_disciplina *raiz)
+// Função para rotacionar um nó para a direita
+arv_disciplina *rotacionarDireita(arv_disciplina *A)
 {
-    // Caso 1: Filho da direita é vermelho
-    if (raiz->dir != NULL && raiz->dir->cor == RED)
-    {
-        raiz = rotacionarEsquerda(raiz);
-        trocaCor(raiz);
-        trocaCor(raiz->esq);
-    }
-
-    // Caso 2: Filho da esquerda e neto esquerdo são vermelhos
-    if (raiz->esq != NULL && raiz->esq->cor == RED && raiz->esq->esq != NULL && raiz->esq->esq->cor == RED)
-    {
-        raiz = rotacionarDireita(raiz);
-        trocaCor(raiz);
-        trocaCor(raiz->dir);
-    }
-
-    // Caso 3: Ambos os filhos são vermelhos
-    if (raiz->esq != NULL && raiz->esq->cor == RED && raiz->dir != NULL && raiz->dir->cor == RED)
-    {
-        trocaCor(raiz);
-        trocaCor(raiz->esq);
-        trocaCor(raiz->dir);
-    }
-
-    return raiz;
+    arv_disciplina *B = A->esq;
+    A->esq = B->dir;
+    B->dir = A;
+    B->cor = A->cor;
+    A->cor = RED;
+    return B;
 }
 
 // Função para inserir uma disciplina na árvore
@@ -95,19 +75,21 @@ arv_disciplina *inserir_disciplina(arv_disciplina *raiz, arv_disciplina *no)
     if (raiz == NULL)
     {
         raiz = no;
-        raiz->cor = RED;
     }
     else
     {
         if (no->dados->codigo < raiz->dados->codigo)
-        {
             raiz->esq = inserir_disciplina(raiz->esq, no);
-        }
         else
-        {
             raiz->dir = inserir_disciplina(raiz->dir, no);
-        }
-        raiz = balancear(raiz);
+
+        // Verificações de correção de árvore vermelho-preta
+        if (raiz->dir != NULL && raiz->dir->cor == RED)
+            raiz = rotacionarEsquerda(raiz);
+        if (raiz->esq != NULL && raiz->esq->cor == RED && raiz->esq->esq != NULL && raiz->esq->esq->cor == RED)
+            raiz = rotacionarDireita(raiz);
+        if (raiz->esq != NULL && raiz->esq->cor == RED && raiz->dir != NULL && raiz->dir->cor == RED)
+            trocaCor(raiz);
     }
     return raiz;
 }
--- Trabalho_segunda_prova/Disciplina.h
+++ Trabalho_segunda_prova/Disciplina.h
@@ -24,7 +24,6 @@ void ler_dados(arv_disciplina *no);
 arv_disciplina *inserir_disciplina(arv_disciplina *raiz, arv_disciplina *no);
 arv_disciplina *rotacionarDireita(arv_disciplina *no);
 arv_disciplina *rotacionarEsquerda(arv_disciplina *no);
-arv_disciplina *rotacionarDireita(arv_disciplina *no);
 void trocaCor(arv_disciplina *no);
 arv_disciplina *balancear(arv_disciplina *raiz);
 int cor(arv_disciplina *no);
