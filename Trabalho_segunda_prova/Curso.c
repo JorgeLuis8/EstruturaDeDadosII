0a590d4adb11f3f1d74ec6d1a888d95be0e0a871
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-06-28 11:22:48 -0300

    atts

--- Trabalho_segunda_prova/Curso.c
+++ Trabalho_segunda_prova/Curso.c
@@ -19,12 +19,12 @@ typedef struct Curso
     struct Dados *dados;
     int cor;
     struct Curso *esq,*dir;
-}curso;
+}arv_curso;
 
 
-curso *cria_no()
+arv_curso *cria_no()
 {
-    curso *no = (curso *)malloc(sizeof(curso));
+    arv_curso *no = (arv_curso *)malloc(sizeof(arv_curso));
     if (no != NULL)
     {
         no->dados = (dados_curso *)malloc(sizeof(dados_curso));
@@ -37,7 +37,7 @@ curso *cria_no()
 }
 
 // Função para ler os dados da disciplina
-void ler_dados(curso *no)
+void ler_dados(arv_curso *no)
 {
     printf("Informe o codigo do curso: ");
     scanf("%d", &no->dados->codigo);
@@ -53,7 +53,7 @@ void ler_dados(curso *no)
 }
 
 // Função para trocar a cor dos nós
-void trocaCor(curso *raiz)
+void trocaCor(arv_curso *raiz)
 {
     if (raiz != NULL)
     {
@@ -66,9 +66,9 @@ void trocaCor(curso *raiz)
 }
 
 // Função para rotacionar um nó para a esquerda
-arv_disciplina *rotacionarEsquerda(arv_disciplina *A)
+arv_curso *rotacionarEsquerda(arv_disciplina *A)
 {
-    arv_disciplina *aux = A->dir;
+    arv_curso *aux = A->dir;
     A->dir = aux->esq;
     aux->esq = A;
     aux->cor = A->cor;
@@ -87,7 +87,7 @@ arv_disciplina *rotacionarDireita(arv_disciplina *A)
     return aux;
 }
 
-curso *inserir_disciplina(curso *raiz, curso *no)
+arv_curso *inserir_disciplina(arv_curso *raiz, arv_curso *no)
 {
     raiz = inserir_rec(raiz, no); // Inserir e corrigir a árvore
     if (raiz != NULL)             // Garantir que a raiz seja sempre preta
@@ -95,7 +95,7 @@ curso *inserir_disciplina(curso *raiz, curso *no)
     return raiz;
 }
 
-arv_disciplina *inserir_rec(curso *raiz, curso *no)
+arv_disciplina *inserir_rec(arv_curso *raiz, arv_curso *no)
 {
     if (raiz == NULL)
     {
@@ -118,3 +118,133 @@ arv_disciplina *inserir_rec(curso *raiz, curso *no)
     return raiz;
 }
 
+void imprimir_disciplinas(arv_curso *raiz)
+{
+    if (raiz != NULL)
+    {
+        imprimir_disciplinas(raiz->esq);
+        printf("Codigo: %d\n", raiz->dados->codigo);
+        printf("Nome: %s\n", raiz->dados->nome);
+        printf("Quantidade de blocos: %d\n", raiz->dados->qtd_blocos);
+        printf("Numero de semnas: %d\n", raiz->dados->num_semanas);
+        printf("Cor: %d\n", raiz->cor);
+        imprimir_disciplinas(raiz->dir);
+    }
+}
+
+arv_curso *buscar_disciplina(arv_curso *raiz, int codigo)
+{
+    if (raiz == NULL)
+        return NULL;
+
+    if (raiz->dados->codigo == codigo)
+        return raiz;
+
+    if (codigo < raiz->dados->codigo)
+        return buscar_disciplina(raiz->esq, codigo);
+    else
+        return buscar_disciplina(raiz->dir, codigo);
+}
+
+arv_curso *move2EsqRED(arv_curso *H)
+{
+    trocaCor(H);
+    if (H->dir != NULL && H->dir->esq != NULL && H->dir->esq->cor == RED)
+    {
+        H->dir = rotacionarDireita(H->dir);
+        H = rotacionarEsquerda(H);
+        trocaCor(H);
+    }
+    return H;
+}
+
+arv_curso *move2DirRED(arv_curso *H)
+{
+    trocaCor(H);
+    if (H->esq != NULL && H->esq->esq != NULL && H->esq->esq->cor == RED)
+    {
+        H = rotacionarDireita(H);
+        trocaCor(H);
+    }
+    return H;
+}
+
+arv_curso *removerMenor(arv_curso *H)
+{
+    if (H->esq == NULL)
+    {
+        free(H);
+        return NULL;
+    }
+    if (H->esq->cor == BLACK && (H->esq->esq == NULL || H->esq->esq->cor == BLACK))
+    {
+        H = move2EsqRED(H);
+    }
+    H->esq = removerMenor(H->esq);
+    return balancear(H);
+}
+
+arv_curso *procuraMenor(arv_curso *atual)
+{
+    arv_curso *no1 = atual;
+    arv_curso *no2 = atual->esq;
+    while (no2 != NULL)
+    {
+        no1 = no2;
+        no2 = no2->esq;
+    }
+    return no1;
+}
+
+arv_curso *remove_NO(arv_curso *H, int valor)
+{
+    if (H == NULL)
+        return NULL;
+
+    if (valor < H->dados->codigo)
+    {
+        if (H->esq != NULL && H->esq->cor == BLACK && (H->esq->esq == NULL || H->esq->esq->cor == BLACK))
+            H = move2EsqRED(H);
+
+        if (H->esq != NULL)
+            H->esq = remove_NO(H->esq, valor);
+    }
+    else
+    {
+        if (H->esq != NULL && H->esq->cor == RED)
+            H = rotacionarDireita(H);
+
+        if (valor == H->dados->codigo && (H->dir == NULL))
+        {
+            free(H);
+            return NULL;
+        }
+
+        if (H->dir != NULL && H->dir->cor == BLACK && (H->dir->esq == NULL || H->dir->esq->cor == BLACK))
+            H = move2DirRED(H);
+
+        if (valor == H->dados->codigo)
+        {
+            arv_disciplina *x = procuraMenor(H->dir);
+            H->dados->codigo = x->dados->codigo;
+            H->dir = removerMenor(H->dir);
+        }
+        else if (H->dir != NULL)
+            H->dir = remove_NO(H->dir, valor);
+    }
+    return balancear(H);
+}
+
+int remove_ArvLLRB(arv_curso **raiz, int codigo)
+{
+    if (buscar_disciplina(*raiz, codigo))
+    {
+        *raiz = remove_NO(*raiz, codigo);
+        if (*raiz != NULL)
+        {
+            (*raiz)->cor = BLACK;
+        }
+        return 1;
+    }
+    return 0;
+}
