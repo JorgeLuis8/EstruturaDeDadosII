d22da2494944e01e0d7dc7c160893c7366172d94
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-07-01 20:32:34 -0300

    atts

---
+++ Trabalho_segunda_prova/2-3/Disciplina.h
@@ -0,0 +1,21 @@
+
+#ifndef DISCIPLINA_H
+#define DISCIPLINA_H
+typedef struct dados_disciplina
+{
+    int codigo;
+    char nome[100];
+    int bloco;
+    int carga_horaria;
+} dados_disciplina;
+
+typedef struct Arv_23Disciplina
+{
+    int ninfos;
+    struct dados_disciplina *dados;
+    struct arv_23Disciplina *esq;
+    struct arv_23Disciplina *meio;
+    struct arv_23Disciplina *dir;
+} arv_23Disciplina;
+
+#endif
