6b9d95f7faf67c8e80c2052eab817b6afd0f6789
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-07-02 19:57:32 -0300

    curso.h

--- Trabalho_segunda_prova/2-3/Curso23.h
+++ Trabalho_segunda_prova/2-3/Curso23.h
@@ -1,32 +1,25 @@
 #ifndef CURSO_H
 #define CURSO_H
+
 #include "Disciplina23.h"
 
-//arvore 2-3
-
-typedef struct dados_curso23
-{
+// Estrutura para armazenar os dados do curso
+typedef struct dados_curso23 {
     int codigo;
     char nome[100];
     int qtd_blocos;
     int num_semanas;
 } dados_curso23;
 
-typedef struct Arv_curso23
-{
-    dados_curso23 *info1,info2;
+// Estrutura da árvore 2-3 para cursos
+typedef struct Arv_curso23 {
+    dados_curso23 *info1;
+    dados_curso23 *info2;
     struct Arv_curso23 *esq;
     struct Arv_curso23 *meio;
     struct Arv_curso23 *dir;
     int num_info;
-    arv_23Disciplina *disciplinas;
-    
+    arv_23Disciplina *disciplinas;  // Lista de disciplinas associadas ao curso
 } arv_curso23;
-
-arv_curso23* cria_no23(dados_curso23* dados);
-arv_curso23* insere_arv23(arv_curso23* raiz, dados_curso23* dados);
-
-
-
 
 #endif /* CURSO_H */
