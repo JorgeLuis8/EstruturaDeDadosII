98c4449b2b82a5747a4fba98b1d7941f01eefca1
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-07-03 13:42:13 -0300

    atts

--- Trabalho_segunda_prova/2-3/main_principal.c
+++ Trabalho_segunda_prova/2-3/main_principal.c
@@ -29,6 +29,7 @@ int main()
         printf("6. Imprimir todos os cursos com a mesma quantidade de blocos\n");
         printf("7. Imprimir dados de uma disciplina dado o codigo da mesma e do curso\n");
         printf("8. Imprimir todas as disciplinas de um determinado bloco de um curso\n");
+        printf("9. Imprimir todas as disciplinas de um curso com a mesma carga horária\n"); // Nova opção adicionada
         printf("0. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -88,8 +89,9 @@ int main()
             }
             imprimir_Disciplina(curso->disciplinas);
             break;
+
         case 5:
-            //Imprimir cursos dado o codigo do mesmo
+            // Imprimir cursos dado o codigo do mesmo
             printf("Imprimir Cursos dado o codigo\n");
             printf("Codigo do Curso: ");
             scanf("%d", &codigoCurso);
@@ -105,15 +107,17 @@ int main()
             printf("Quantidade de Blocos: %d\n", curso->info1->qtd_blocos);
             printf("Numero de Semanas: %d\n", curso->info1->num_semanas);
             break;
+
         case 6:
-            //Imprimir todos os cursos com a mesma quantidade de blocos
+            // Imprimir todos os cursos com a mesma quantidade de blocos
             printf("Imprimir Cursos dado a quantidade de blocos\n");
             printf("Quantidade de Blocos: ");
             scanf("%d", &qtd_blocos);
             imprimirCursoBlocos(raizCurso, qtd_blocos);
             break;
+
         case 7:
-            //Imprimir dados de uma disciplina dado o codigo da mesma e do curso
+            // Imprimir dados de uma disciplina dado o codigo da mesma e do curso
             printf("Imprimir Disciplina dado o codigo\n");
             printf("Codigo do Curso: ");
             scanf("%d", &codigoCurso);
@@ -137,8 +141,9 @@ int main()
             printf("Bloco: %d\n", disciplina->info1->bloco);
             printf("Carga Horaria: %d\n", disciplina->info1->carga_horaria);
             break;
+
         case 8:
-            //Imprimir todas as disciplinas de um determinado bloco de um curso
+            // Imprimir todas as disciplinas de um determinado bloco de um curso
             printf("Imprimir Disciplinas dado o bloco\n");
             printf("Codigo do Curso: ");
             scanf("%d", &codigoCurso);
@@ -158,8 +163,27 @@ int main()
             }
             imprimirDisciplinasBloco(curso->disciplinas, bloco);
             break;
+
+        case 9:
+            // Imprimir todas as disciplinas de um curso com a mesma carga horária
+            printf("Imprimir Disciplinas com a mesma carga horária\n");
+            printf("Codigo do Curso: ");
+            scanf("%d", &codigoCurso);
+            // Aqui você deve encontrar o curso na árvore
+            curso = buscarCurso(raizCurso, codigoCurso);
+            if (curso == NULL)
+            {
+                printf("Curso não encontrado!\n");
+                break;
+            }
+            printf("Carga Horaria: ");
+            scanf("%d", &carga_horaria);
+            imprimirDisciplinasCargaHoraria(curso->disciplinas, carga_horaria);
+            break;
+
         case 0:
             exit(0);
+
         default:
             printf("Opcao invalida!\n");
         }
