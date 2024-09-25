08b7afc5ce11018e1c06969047dc40f7219c598b
Jorge Luis <jorge.luz@ufpi.edu.br>
2024-09-24 21:18:37 -0300

    atts do menu

--- Trabalho_primeira_prova - Copia/Avlv2/teste.c
+++ Trabalho_primeira_prova - Copia/Avlv2/teste.c
@@ -4,7 +4,7 @@
 #include <windows.h>
 #include "notas.c"
 
-// Função para medir o tempo no Windows
+// Funcao para medir o tempo no Windows
 void iniciar_medicao(LARGE_INTEGER *inicio, LARGE_INTEGER *freq) {
     QueryPerformanceFrequency(freq);
     QueryPerformanceCounter(inicio);
@@ -12,41 +12,203 @@ void iniciar_medicao(LARGE_INTEGER *inicio, LARGE_INTEGER *freq) {
 
 long long finalizar_medicao(LARGE_INTEGER inicio, LARGE_INTEGER fim, LARGE_INTEGER freq) {
     QueryPerformanceCounter(&fim);
-    return (fim.QuadPart - inicio.QuadPart) * 1000000000 / freq.QuadPart;
+    return (fim.QuadPart - inicio.QuadPart) * 1000000000 / freq.QuadPart; // Converte para nanosegundos
 }
 
-int main() {
-    arvore_notas *raiz = NULL;
-    srand((unsigned)time(NULL)); // Inicializa o gerador de números aleatórios
+// Funcao para desalocar a arvore de notas
+void liberar_arvore(arvore_notas *raiz) {
+    if (raiz == NULL) {
+        return;
+    }
+    liberar_arvore(raiz->esq);
+    liberar_arvore(raiz->dir);
+    free(raiz); // Libera o no atual
+}
 
+// Caso para numeros ordenados
+void caso_numeros_ordenados() {
     LARGE_INTEGER inicio, fim, freq;
     long long tempo_total_insercao = 0;
     long long tempo_total_busca = 0;
 
-    // Inserir 30 elementos
-    for (int i = 0; i < 30; i++) {
-        arvore_notas *nova_nota = criar_nota();
-        nova_nota->codigo_disciplina = rand() % 100; // Código aleatório de 0 a 99
-        snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
-        nova_nota->nota_final = (float)(rand() % 101); // Nota aleatória de 0 a 100
+    int num_execucoes = 30;
+    int num_insercoes = 1000;
 
-        iniciar_medicao(&inicio, &freq);
-        raiz = inserir_nota(raiz, nova_nota);
-        tempo_total_insercao += finalizar_medicao(inicio, fim, freq);
+    for (int j = 0; j < num_execucoes; j++) {
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
+        long long tempo_insercao_atual = 0;
+
+        // Inserir 1000 elementos
+        for (int i = 0; i < num_insercoes; i++) {
+            arvore_notas *nova_nota = criar_nota();
+            nova_nota->codigo_disciplina = i; // Codigo em ordem crescente
+            snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
+
+            iniciar_medicao(&inicio, &freq);
+            raiz = inserir_nota(raiz, nova_nota);
+            tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Somar o tempo total de insercao ao total geral
+        tempo_total_insercao += tempo_insercao_atual;
+
+        // Buscar o mesmo elemento 30 vezes e medir o tempo
+        for (int i = 0; i < num_execucoes; i++) {
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
+
+            iniciar_medicao(&inicio, &freq);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
+            tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Limpar a arvore ao final de cada iteracao
+        liberar_arvore(raiz);
     }
 
-    // Medir tempo de busca
-    for (int i = 0; i < 30; i++) {
-        int codigo_busca = rand() % 100; // Código a ser buscado
+    // Calcular a media de tempo de insercao e busca
+    long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
+    long long media_tempo_busca = tempo_total_busca / num_execucoes;
 
-        iniciar_medicao(&inicio, &freq);
-        // Código de busca aqui
-        // Exemplo: arvore_notas *resultado = buscar_nota(raiz, codigo_busca);
-        tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+    // Mostrar a media de insercao e busca
+    printf("Numeros Ordenados:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
+}
+
+// Caso para numeros decrescentes
+void caso_numeros_decrescentes() {
+    LARGE_INTEGER inicio, fim, freq;
+    long long tempo_total_insercao = 0;
+    long long tempo_total_busca = 0;
+
+    int num_execucoes = 30;
+    int num_insercoes = 1000;
+
+    for (int j = 0; j < num_execucoes; j++) {
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
+        long long tempo_insercao_atual = 0;
+
+        // Inserir 1000 elementos do maior para o menor
+        for (int i = num_insercoes - 1; i >= 0; i--) {  // Insercao decrescente
+            arvore_notas *nova_nota = criar_nota();
+            nova_nota->codigo_disciplina = i; // Codigo em ordem decrescente
+            snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
+
+            iniciar_medicao(&inicio, &freq);
+            raiz = inserir_nota(raiz, nova_nota);
+            tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Somar o tempo total de insercao ao total geral
+        tempo_total_insercao += tempo_insercao_atual;
+
+        // Buscar o mesmo elemento 30 vezes e medir o tempo
+        for (int i = 0; i < num_execucoes; i++) {
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
+
+            iniciar_medicao(&inicio, &freq);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
+            tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Limpar a arvore ao final de cada iteracao
+        liberar_arvore(raiz);
     }
 
-    printf("Tempo total de inserção: %lld nanosegundos\n", tempo_total_insercao);
-    printf("Tempo total de busca: %lld nanosegundos\n", tempo_total_busca);
+    // Calcular a media de tempo de insercao e busca
+    long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
+    long long media_tempo_busca = tempo_total_busca / num_execucoes;
 
-    return 0;
+    // Mostrar a media de insercao e busca
+    printf("Numeros Decrescentes:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
+}
+
+// Caso para numeros aleatorios
+void caso_numeros_aleatorios() {
+    LARGE_INTEGER inicio, fim, freq;
+    long long tempo_total_insercao = 0;
+    long long tempo_total_busca = 0;
+
+    int num_execucoes = 30;
+    int num_insercoes = 1000;
+
+    for (int j = 0; j < num_execucoes; j++) {
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
+        long long tempo_insercao_atual = 0;
+
+        // Inserir 1000 elementos com codigos de disciplina aleatorios
+        for (int i = 0; i < num_insercoes; i++) {  // Insercao aleatoria
+            arvore_notas *nova_nota = criar_nota();
+            nova_nota->codigo_disciplina = rand() % num_insercoes; // Codigo aleatorio
+            snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
+
+            iniciar_medicao(&inicio, &freq);
+            raiz = inserir_nota(raiz, nova_nota);
+            tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Somar o tempo total de insercao ao total geral
+        tempo_total_insercao += tempo_insercao_atual;
+
+        // Buscar o mesmo elemento 30 vezes e medir o tempo
+        for (int i = 0; i < num_execucoes; i++) {
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
+
+            iniciar_medicao(&inicio, &freq);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
+            tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Limpar a arvore ao final de cada iteracao
+        liberar_arvore(raiz);
+    }
+
+    // Calcular a media de tempo de insercao e busca
+    long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
+    long long media_tempo_busca = tempo_total_busca / num_execucoes;
+
+    // Mostrar a media de insercao e busca
+    printf("Numeros Aleatorios:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
+}
+
+
+// Funcao principal
+int main() {
+    int opcao;
+
+    do {
+        // Exibir o menu
+        printf("Selecionar caso de teste:\n");
+        printf("1 - Numeros Ordenados\n");
+        printf("2 - Numeros Decrescentes\n");
+        printf("3 - Numeros Aleatorios\n");
+        printf("0 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        // Executar a opcao escolhida
+        switch (opcao) {
+            case 1:
+                caso_numeros_ordenados();
+                break;
+            case 2:
+                caso_numeros_decrescentes();
+                break;
+            case 3:
+                caso_numeros_aleatorios();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    }while (opcao != 0);
 }
--- Trabalho_primeira_prova - Copia/Binariav2/teste.c
+++ Trabalho_primeira_prova - Copia/Binariav2/teste.c
@@ -1,199 +1,10 @@
-//////////////////////////////////////// Numeros ordenados ////////////////////////////////////////
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <windows.h>
-// #include "notas.c"
-
-// // Função para medir o tempo no Windows
-// void iniciar_medicao(LARGE_INTEGER *inicio, LARGE_INTEGER *freq) {
-//     QueryPerformanceFrequency(freq);
-//     QueryPerformanceCounter(inicio);
-// }
-
-// long long finalizar_medicao(LARGE_INTEGER inicio, LARGE_INTEGER fim, LARGE_INTEGER freq) {
-//     QueryPerformanceCounter(&fim);
-//     return (fim.QuadPart - inicio.QuadPart) * 1000000000 / freq.QuadPart; // Converte para nanosegundos
-// }
-
-// // Função para desalocar a árvore de notas
-// void liberar_arvore(arvore_notas *raiz) {
-//     if (raiz == NULL) {
-//         return;
-//     }
-//     liberar_arvore(raiz->esq);
-//     liberar_arvore(raiz->dir);
-//     free(raiz); // Libera o nó atual
-// }
-
-// // Função para buscar uma nota pelo código da disciplina
-
-
-// int main() {
-//     srand((unsigned)time(NULL)); // Inicializa o gerador de números aleatórios
-
-//     LARGE_INTEGER inicio, fim, freq;
-//     long long tempo_total_insercao = 0;
-//     long long tempo_total_busca = 0;
-
-//     int num_execucoes = 30;
-//     int num_insercoes = 1000;
-
-//     // Repetir o processo de inserção e busca 30 vezes
-//     for (int j = 0; j < num_execucoes; j++) {
-//         arvore_notas *raiz = NULL; // Reinicializa a árvore a cada iteração
-//         long long tempo_insercao_atual = 0;
-
-//         // Inserir 1000 elementos
-//         for (int i = 0; i < num_insercoes; i++) {
-//             arvore_notas *nova_nota = criar_nota();
-//             nova_nota->codigo_disciplina = i; // Código em ordem crescente
-//             snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
-//             nova_nota->nota_final = (float)(rand() % 101); // Nota aleatória de 0 a 100
-
-//             iniciar_medicao(&inicio, &freq);
-//             raiz = inserir_nota(raiz, nova_nota);
-//             QueryPerformanceCounter(&fim);
-//             tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
-//         }
-
-//         // Somar o tempo total de inserção ao total geral
-//         tempo_total_insercao += tempo_insercao_atual;
-
-//         // Buscar o mesmo elemento 30 vezes e medir o tempo
-//         for (int i = 0; i < num_execucoes; i++) {
-//             int codigo_busca = rand() % num_insercoes; // Código a ser buscado
-
-//             iniciar_medicao(&inicio, &freq);
-//             arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
-//             QueryPerformanceCounter(&fim);
-//             tempo_total_busca += finalizar_medicao(inicio, fim, freq);
-//         }
-
-//         // Limpar a árvore ao final de cada iteração
-//         liberar_arvore(raiz);
-//     }
-
-//     // Calcular a média de tempo de inserção e busca
-//     long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
-//     long long media_tempo_busca = tempo_total_busca / num_execucoes;
-
-//     // Mostrar a média de inserção e busca
-//     printf("Tempo médio de inserção: %lld nanosegundos\n", media_tempo_insercao);
-//     printf("Tempo médio de busca: %lld nanosegundos\n", media_tempo_busca);
-
-//     return 0;
-// }
-//////////////////////////////////////// Numeros ordenados ////////////////////////////////////////
-
-
-
-//////////////////////////////////////// Numeros decrecente ////////////////////////////////////////
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <windows.h>
-// #include "notas.c"
-
-// // Função para medir o tempo no Windows
-// void iniciar_medicao(LARGE_INTEGER *inicio, LARGE_INTEGER *freq) {
-//     QueryPerformanceFrequency(freq);
-//     QueryPerformanceCounter(inicio);
-// }
-
-// long long finalizar_medicao(LARGE_INTEGER inicio, LARGE_INTEGER fim, LARGE_INTEGER freq) {
-//     QueryPerformanceCounter(&fim);
-//     return (fim.QuadPart - inicio.QuadPart) * 1000000000 / freq.QuadPart; // Converte para nanosegundos
-// }
-
-// // Função para desalocar a árvore de notas
-// void liberar_arvore(arvore_notas *raiz) {
-//     if (raiz == NULL) {
-//         return;
-//     }
-//     liberar_arvore(raiz->esq);
-//     liberar_arvore(raiz->dir);
-//     free(raiz); // Libera o nó atual
-// }
-
-// // Função para buscar uma nota pelo código da disciplina
-// arvore_notas* buscar_nota(arvore_notas *raiz, int codigo_disciplina) {
-//     if (raiz == NULL || raiz->codigo_disciplina == codigo_disciplina) {
-//         return raiz; // Encontrado ou árvore vazia
-//     }
-//     if (codigo_disciplina < raiz->codigo_disciplina) {
-//         return buscar_nota(raiz->esq, codigo_disciplina); // Busca na subárvore esquerda
-//     }
-//     return buscar_nota(raiz->dir, codigo_disciplina); // Busca na subárvore direita
-// }
-
-// int main() {
-//     srand((unsigned)time(NULL)); // Inicializa o gerador de números aleatórios
-
-//     LARGE_INTEGER inicio, fim, freq;
-//     long long tempo_total_insercao = 0;
-//     long long tempo_total_busca = 0;
-
-//     int num_execucoes = 30;
-//     int num_insercoes = 1000;
-
-//     // Repetir o processo de inserção e busca 30 vezes
-//     for (int j = 0; j < num_execucoes; j++) {
-//         arvore_notas *raiz = NULL; // Reinicializa a árvore a cada iteração
-//         long long tempo_insercao_atual = 0;
-
-//         // Inserir 1000 elementos do maior para o menor
-//         for (int i = num_insercoes - 1; i >= 0; i--) {  // Inserção decrescente
-//             arvore_notas *nova_nota = criar_nota();
-//             nova_nota->codigo_disciplina = i; // Código em ordem decrescente
-//             snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
-//             nova_nota->nota_final = (float)(rand() % 101); // Nota aleatória de 0 a 100
-
-//             iniciar_medicao(&inicio, &freq);
-//             raiz = inserir_nota(raiz, nova_nota);
-//             QueryPerformanceCounter(&fim);
-//             tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
-//         }
-
-//         // Somar o tempo total de inserção ao total geral
-//         tempo_total_insercao += tempo_insercao_atual;
-
-//         // Buscar o mesmo elemento 30 vezes e medir o tempo
-//         for (int i = 0; i < num_execucoes; i++) {
-//             int codigo_busca = rand() % num_insercoes; // Código a ser buscado
-
-//             iniciar_medicao(&inicio, &freq);
-//             arvore_notas *resultado = buscar_nota(raiz, codigo_busca);
-//             QueryPerformanceCounter(&fim);
-//             tempo_total_busca += finalizar_medicao(inicio, fim, freq);
-//         }
-
-//         // Limpar a árvore ao final de cada iteração
-//         liberar_arvore(raiz);
-//     }
-
-//     // Calcular a média de tempo de inserção e busca
-//     long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
-//     long long media_tempo_busca = tempo_total_busca / num_execucoes;
-
-//     // Mostrar a média de inserção e busca
-//     printf("Tempo médio de inserção: %lld nanosegundos\n", media_tempo_insercao);
-//     printf("Tempo médio de busca: %lld nanosegundos\n", media_tempo_busca);
-
-//     return 0;
-// }
-
-//////////////////////////////////////// Numeros decrecente ////////////////////////////////////////
-
-//////////////////////////////////////// Numeros aleatorios ////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
 #include "notas.c"
 
-// Função para medir o tempo no Windows
+// Funcao para medir o tempo no Windows
 void iniciar_medicao(LARGE_INTEGER *inicio, LARGE_INTEGER *freq) {
     QueryPerformanceFrequency(freq);
     QueryPerformanceCounter(inicio);
@@ -204,30 +15,18 @@ long long finalizar_medicao(LARGE_INTEGER inicio, LARGE_INTEGER fim, LARGE_INTEG
     return (fim.QuadPart - inicio.QuadPart) * 1000000000 / freq.QuadPart; // Converte para nanosegundos
 }
 
-// Função para desalocar a árvore de notas
+// Funcao para desalocar a arvore de notas
 void liberar_arvore(arvore_notas *raiz) {
     if (raiz == NULL) {
         return;
     }
     liberar_arvore(raiz->esq);
     liberar_arvore(raiz->dir);
-    free(raiz); // Libera o nó atual
+    free(raiz); // Libera o no atual
 }
 
-// Função para buscar uma nota pelo código da disciplina
-arvore_notas* buscar_nota(arvore_notas *raiz, int codigo_disciplina) {
-    if (raiz == NULL || raiz->codigo_disciplina == codigo_disciplina) {
-        return raiz; // Encontrado ou árvore vazia
-    }
-    if (codigo_disciplina < raiz->codigo_disciplina) {
-        return buscar_nota(raiz->esq, codigo_disciplina); // Busca na subárvore esquerda
-    }
-    return buscar_nota(raiz->dir, codigo_disciplina); // Busca na subárvore direita
-}
-
-int main() {
-    srand((unsigned)time(NULL)); // Inicializa o gerador de números aleatórios
-
+// Caso para numeros ordenados
+void caso_numeros_ordenados() {
     LARGE_INTEGER inicio, fim, freq;
     long long tempo_total_insercao = 0;
     long long tempo_total_busca = 0;
@@ -235,49 +34,181 @@ int main() {
     int num_execucoes = 30;
     int num_insercoes = 1000;
 
-    // Repetir o processo de inserção e busca 30 vezes
     for (int j = 0; j < num_execucoes; j++) {
-        arvore_notas *raiz = NULL; // Reinicializa a árvore a cada iteração
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
         long long tempo_insercao_atual = 0;
 
-        // Inserir 1000 elementos com códigos de disciplina aleatórios
-        for (int i = 0; i < num_insercoes; i++) {  // Inserção aleatória
+        // Inserir 1000 elementos
+        for (int i = 0; i < num_insercoes; i++) {
             arvore_notas *nova_nota = criar_nota();
-            nova_nota->codigo_disciplina = rand() % num_insercoes; // Código aleatório
+            nova_nota->codigo_disciplina = i; // Codigo em ordem crescente
             snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
-            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatória de 0 a 100
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
 
             iniciar_medicao(&inicio, &freq);
             raiz = inserir_nota(raiz, nova_nota);
-            QueryPerformanceCounter(&fim);
             tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
         }
 
-        // Somar o tempo total de inserção ao total geral
+        // Somar o tempo total de insercao ao total geral
         tempo_total_insercao += tempo_insercao_atual;
 
         // Buscar o mesmo elemento 30 vezes e medir o tempo
         for (int i = 0; i < num_execucoes; i++) {
-            int codigo_busca = rand() % num_insercoes; // Código a ser buscado
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
 
             iniciar_medicao(&inicio, &freq);
-            arvore_notas *resultado = buscar_nota(raiz, codigo_busca);
-            QueryPerformanceCounter(&fim);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
             tempo_total_busca += finalizar_medicao(inicio, fim, freq);
         }
 
-        // Limpar a árvore ao final de cada iteração
+        // Limpar a arvore ao final de cada iteracao
         liberar_arvore(raiz);
     }
 
-    // Calcular a média de tempo de inserção e busca
+    // Calcular a media de tempo de insercao e busca
     long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
     long long media_tempo_busca = tempo_total_busca / num_execucoes;
 
-    // Mostrar a média de inserção e busca
-    printf("Tempo médio de inserção: %lld nanosegundos\n", media_tempo_insercao);
-    printf("Tempo médio de busca: %lld nanosegundos\n", media_tempo_busca);
-
-    return 0;
+    // Mostrar a media de insercao e busca
+    printf("Numeros Ordenados:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
 }
-//////////////////////////////////////// Numeros aleatorios ////////////////////////////////////////
+
+// Caso para numeros decrescentes
+void caso_numeros_decrescentes() {
+    LARGE_INTEGER inicio, fim, freq;
+    long long tempo_total_insercao = 0;
+    long long tempo_total_busca = 0;
+
+    int num_execucoes = 30;
+    int num_insercoes = 1000;
+
+    for (int j = 0; j < num_execucoes; j++) {
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
+        long long tempo_insercao_atual = 0;
+
+        // Inserir 1000 elementos do maior para o menor
+        for (int i = num_insercoes - 1; i >= 0; i--) {  // Insercao decrescente
+            arvore_notas *nova_nota = criar_nota();
+            nova_nota->codigo_disciplina = i; // Codigo em ordem decrescente
+            snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
+
+            iniciar_medicao(&inicio, &freq);
+            raiz = inserir_nota(raiz, nova_nota);
+            tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Somar o tempo total de insercao ao total geral
+        tempo_total_insercao += tempo_insercao_atual;
+
+        // Buscar o mesmo elemento 30 vezes e medir o tempo
+        for (int i = 0; i < num_execucoes; i++) {
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
+
+            iniciar_medicao(&inicio, &freq);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
+            tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Limpar a arvore ao final de cada iteracao
+        liberar_arvore(raiz);
+    }
+
+    // Calcular a media de tempo de insercao e busca
+    long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
+    long long media_tempo_busca = tempo_total_busca / num_execucoes;
+
+    // Mostrar a media de insercao e busca
+    printf("Numeros Decrescentes:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
+}
+
+// Caso para numeros aleatorios
+void caso_numeros_aleatorios() {
+    LARGE_INTEGER inicio, fim, freq;
+    long long tempo_total_insercao = 0;
+    long long tempo_total_busca = 0;
+
+    int num_execucoes = 30;
+    int num_insercoes = 1000;
+
+    for (int j = 0; j < num_execucoes; j++) {
+        arvore_notas *raiz = NULL; // Reinicializa a arvore a cada iteracao
+        long long tempo_insercao_atual = 0;
+
+        // Inserir 1000 elementos com codigos de disciplina aleatorios
+        for (int i = 0; i < num_insercoes; i++) {  // Insercao aleatoria
+            arvore_notas *nova_nota = criar_nota();
+            nova_nota->codigo_disciplina = rand() % num_insercoes; // Codigo aleatorio
+            snprintf(nova_nota->semestre_cursado, sizeof(nova_nota->semestre_cursado), "2023-2"); // Exemplo de semestre
+            nova_nota->nota_final = (float)(rand() % 101); // Nota aleatoria de 0 a 100
+
+            iniciar_medicao(&inicio, &freq);
+            raiz = inserir_nota(raiz, nova_nota);
+            tempo_insercao_atual += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Somar o tempo total de insercao ao total geral
+        tempo_total_insercao += tempo_insercao_atual;
+
+        // Buscar o mesmo elemento 30 vezes e medir o tempo
+        for (int i = 0; i < num_execucoes; i++) {
+            int codigo_busca = rand() % num_insercoes; // Codigo a ser buscado
+
+            iniciar_medicao(&inicio, &freq);
+            arvore_notas *resultado = buscar_notas(raiz, codigo_busca);
+            tempo_total_busca += finalizar_medicao(inicio, fim, freq);
+        }
+
+        // Limpar a arvore ao final de cada iteracao
+        liberar_arvore(raiz);
+    }
+
+    // Calcular a media de tempo de insercao e busca
+    long long media_tempo_insercao = tempo_total_insercao / num_execucoes;
+    long long media_tempo_busca = tempo_total_busca / num_execucoes;
+
+    // Mostrar a media de insercao e busca
+    printf("Numeros Aleatorios:\n");
+    printf("Tempo medio de insercao: %lld nanosegundos\n", media_tempo_insercao);
+    printf("Tempo medio de busca: %lld nanosegundos\n\n", media_tempo_busca);
+}
+
+
+// Funcao principal
+int main() {
+    int opcao;
+
+    do {
+        // Exibir o menu
+        printf("Selecionar caso de teste:\n");
+        printf("1 - Numeros Ordenados\n");
+        printf("2 - Numeros Decrescentes\n");
+        printf("3 - Numeros Aleatorios\n");
+        printf("0 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        // Executar a opcao escolhida
+        switch (opcao) {
+            case 1:
+                caso_numeros_ordenados();
+                break;
+            case 2:
+                caso_numeros_decrescentes();
+                break;
+            case 3:
+                caso_numeros_aleatorios();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    }while (opcao != 0);
+}
